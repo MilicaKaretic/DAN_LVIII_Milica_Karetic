cb711279e9120338906e0ec75eb60235c992b569
MilicaKaretic <milicakaretic1@gmail.com>
2020-08-31 13:14:46 +0200

    implement game

--- DAN_LVIII_Milica_Karetic/Game.cs
+++ DAN_LVIII_Milica_Karetic/Game.cs
@@ -6,7 +6,387 @@ using System.Threading.Tasks;
 
 namespace DAN_LVIII_Milica_Karetic
 {
+    /// <summary>
+    /// Class for game simulation
+    /// </summary>
     class Game
     {
+
+        public bool PlayerTurn { get; set; }
+        public bool GameState { get; private set; }
+        private int maxSize { get; set; } = 9;
+        private bool foundWinPattern { get; set; }
+        public short maxRowSize { get; private set; } = 3;
+        public short maxcolSize { get; private set; } = 3;
+
+        //win pattern (length 3)
+        public short[] winSegments;
+
+        //winner
+        public IdentifyWinner winner;
+
+
+        /// <summary>
+        /// state of board spots - empty, x or o
+        /// </summary>
+        public enum SpotState
+        {
+            empty,
+            X,
+            O
+        }
+
+        /// <summary>
+        /// winner options
+        /// </summary>
+        public enum IdentifyWinner
+        {
+            NULL,
+            draw,
+            player,
+            computer
+            
+        }
+
+        public SpotState[] spotValues;
+
+        /// <summary>
+        /// Game constructor
+        /// </summary>
+        public Game()
+        {
+            winSegments = new short[3] { 0, 0, 0 };
+            spotValues = new SpotState[9];
+            winner = new IdentifyWinner();
+            foundWinPattern = false;
+            PlayerTurn = true;
+            GameState = true;
+        }
+
+        /// <summary>
+        /// initialize board with all empty spots
+        /// </summary>
+        public void InitialBoard()
+        {
+            //all empty spots
+            for (int i = 0; i < maxSize; ++i)
+            {
+                spotValues[i] = SpotState.empty;
+            }
+            //first turn is player's turn
+            winner = IdentifyWinner.NULL;
+            foundWinPattern = false;
+            PlayerTurn = true;
+            GameState = true;
+        }
+
+
+        /// <summary>
+        /// computer playing
+        /// </summary>
+        /// <returns></returns>
+        public int computerPlay()
+        {
+
+            int index = 0;
+
+            short? val = CPUPlay();
+
+            if (val != null)
+            {
+                index = (int)val;
+                return index;
+            }
+
+            //getting random spot until it matches the empty one
+            while (true)
+            {
+                Random random = new Random();
+                index = random.Next(0, 8);
+
+                if (spotValues[index] == SpotState.empty)
+                    break;
+            }
+
+            return index;
+        }
+
+
+        /// <summary>
+        /// Checks if someone won
+        /// </summary>
+        public void checkGameState()
+        {
+            if (winner == IdentifyWinner.NULL)
+            {
+                //if there is empty spots game continues else game is over
+                foreach (var spot in spotValues)
+                {
+                    if (spot == Game.SpotState.empty)
+                    {
+                        GameState = true;
+                        break;
+                    }
+                    else
+                        GameState = false;
+                }
+
+                //if there is no empty spots and winner is null -> it's draw
+                if (GameState != true)
+                    winner = IdentifyWinner.draw;
+
+            }
+        }
+
+        /// <summary>
+        /// Get winner of game
+        /// </summary>
+        /// <param name="tempstate"></param>
+        public void getWinner(SpotState spotstate)
+        {
+            if (GameState != false)
+            {
+                getWin(spotstate);
+
+                if (foundWinPattern)
+                {
+                    if (spotstate == SpotState.X)
+                        winner = IdentifyWinner.player;
+
+                    else if (spotstate == SpotState.O)
+                        winner = IdentifyWinner.computer;
+                }
+
+                checkGameState();
+            }
+
+        }
+
+        /// <summary>
+        /// method for computers turn in game
+        /// </summary>
+        /// <returns></returns>
+        private short? CPUPlay()
+        {
+
+            short count = 0;
+            short? CPUval = null;
+
+            #region Horizontal
+
+            short temp = maxRowSize;
+            short temp2 = 0;
+
+            for (short j = 0; j < maxRowSize; ++j)
+            {
+                for (short i = temp2; i < temp; ++i)
+                {
+                    if (spotValues[i] == SpotState.X)
+                        ++count;
+                    else
+                        CPUval = i;
+                }
+
+                if (count == 2 && spotValues[(int)CPUval] != SpotState.O)
+                    break;
+
+                count = 0;
+                temp += maxRowSize;
+                temp2 += 2 + 1;
+            }
+
+            if (count == 2 && CPUval != null)
+                return (short)CPUval;
+
+            #endregion
+
+            #region Vertical
+
+            count = 0;
+            temp = 6;
+            temp2 = 0;
+            short temp3 = temp2;
+
+            for (short j = 0; j < maxcolSize; ++j)
+            {
+                for (short i = temp2; i <= temp; i += 3)
+                {
+                    if (spotValues[i] == SpotState.X)
+                        ++count;
+                    else
+                        CPUval = i;
+                }
+
+                if (count == 2)
+                    break;
+
+                count = 0;
+                temp += 1;
+                temp3 += 1;
+                temp2 = temp3;
+            }
+
+            if (count == 2 && CPUval != null)
+                return (short)CPUval;
+
+            #endregion
+
+            #region diagonal
+
+            count = 0;
+            temp = 8;
+            temp2 = 0;
+            short incr = 4;
+
+            for (short j = 0; j < maxcolSize - 1; ++j)
+            {
+                for (short i = temp2; i <= temp; i += incr)
+                {
+                    if (spotValues[i] == SpotState.X)
+                        ++count;
+                    else
+                        CPUval = i;
+                }
+
+                if (count == 2)
+                    break;
+
+                count = 0;
+                temp -= 2;
+                temp2 += 2;
+                incr -= 2;
+            }
+
+            if (count == 2 && CPUval != null) return (short)CPUval;
+
+            #endregion
+
+            return null;
+        }
+
+        /// <summary>
+        /// chechs if someone win
+        /// </summary>
+        /// <param name="boxState"></param>
+        private void getWin(SpotState boxState)
+        {
+
+            #region Horizontalcheck
+
+            short segindex = 0;
+            short temp = maxRowSize;
+            short temp2 = 0;
+
+            for (short j = 0; j < maxRowSize; ++j)
+            {
+                for (short i = temp2; i < temp; ++i)
+                {
+                    if (spotValues[i] != boxState)
+                    {
+                        GameState = true;
+                        foundWinPattern = false;
+                        break;
+                    }
+                    else if (spotValues[i] == boxState)
+                    {
+                        winSegments[segindex++] = i;
+                        GameState = false;
+                        foundWinPattern = true;
+                    }
+                }
+
+                if (foundWinPattern)
+                    break;
+
+                segindex = 0;
+                temp += maxRowSize;
+                temp2 += 2 + 1;
+            }
+
+            #endregion
+
+            if (foundWinPattern)
+                return;
+
+            #region VerticalCheck
+
+            segindex = 0;
+            temp = 6;
+            temp2 = 0;
+            short temp3 = temp2;
+
+            for (short j = 0; j < maxcolSize; ++j)
+            {
+                for (short i = temp2; i <= temp; i += 3)
+                {
+                    if (spotValues[i] != boxState)
+                    {
+                        GameState = true;
+                        foundWinPattern = false;
+                        break;
+                    }
+
+                    else if (spotValues[i] == boxState)
+                    {
+                        winSegments[segindex++] = i;
+                        GameState = false;
+                        foundWinPattern = true;
+                    }
+                }
+
+                if (foundWinPattern)
+                    break;
+
+                segindex = 0;
+                temp += 1;
+                temp3 += 1;
+                temp2 = temp3;
+            }
+
+            #endregion
+
+            //if winner pattern for horizontal and vertical patterns is found return
+            if (foundWinPattern)
+                return;
+
+            //chech diagonals
+            #region diagonalCheck
+
+            segindex = 0;
+            temp = 8;
+            temp2 = 0;
+            short incr = 4;
+
+            for (short j = 0; j < maxcolSize - 1; ++j)
+            {
+                for (short i = temp2; i <= temp; i += incr)
+                {
+                    if (spotValues[i] != boxState)
+                    {
+                        GameState = true;
+                        foundWinPattern = false;
+                        break;
+                    }
+                    else if (spotValues[i] == boxState)
+                    {
+                        winSegments[segindex++] = i;
+                        GameState = false;
+                        foundWinPattern = true;
+                    }
+                }
+
+                //if pattern is found
+                if (foundWinPattern)
+                    break;
+
+                segindex = 0;
+                temp -= 2;
+                temp2 += 2;
+                incr -= 2;
+            }
+            #endregion
+        }
+
+
     }
 }
--- DAN_LVIII_Milica_Karetic/MainWindow.xaml.cs
+++ DAN_LVIII_Milica_Karetic/MainWindow.xaml.cs
@@ -20,9 +20,101 @@ namespace DAN_LVIII_Milica_Karetic
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Game game;
+
         public MainWindow()
         {
             InitializeComponent();
+            game = new Game();
+
+            newGame();
+        }
+
+        private void newGame()
+        {
+            game.InitialBoard();
+
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+            });
+        }
+
+        /// <summary>
+        /// event handler for button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            //get row and column from button clicked
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var gridIndex = column + (row * 3);
+
+            //if it's players turn
+            if (game.PlayerTurn && game.GameState)
+            {
+                //if spot isn't empty
+                if (game.spotValues[gridIndex] != Game.SpotState.empty)
+                {
+                    MessageBox.Show("Spot is taken!");
+                }
+                //else if it is empty and its players turn 
+                else
+                {
+                    game.spotValues[gridIndex] = Game.SpotState.X;
+                    button.Content = 'X';
+                    game.PlayerTurn = false;
+                }
+                game.getWinner(Game.SpotState.X);
+            }
+
+            //if it is computer's turn
+            if (!game.PlayerTurn && game.GameState)
+            {
+                var btn = Container.Children.Cast<Button>().ToArray();
+                var index = game.computerPlay();
+                btn[index].Content = 'O';
+
+                game.spotValues[index] = Game.SpotState.O;
+                game.PlayerTurn = true;
+                game.getWinner(Game.SpotState.O);
+            }
+
+            if (game.GameState != true)
+            {
+                //if someone won
+                if (game.winner == Game.IdentifyWinner.player || game.winner == Game.IdentifyWinner.computer)
+                {
+                    var btn = Container.Children.Cast<Button>().ToArray();
+
+                    for (int i = 0; i < game.maxRowSize; ++i)
+                    {
+                        btn[game.winSegments[i]].Background = Brushes.Green;
+                    }
+                    MessageBox.Show(game.winner.ToString() + " won!");
+                    this.newGame();
+                }
+
+                if (game.winner == Game.IdentifyWinner.draw)
+                {
+                    Container.Children.Cast<Button>().ToList().ForEach(btn =>
+                    {
+                        button.Foreground = Brushes.Black;
+                    });
+
+                    MessageBox.Show("It's " + game.winner.ToString());
+                    this.newGame();
+                }
+
+
+            }
         }
     }
 }
